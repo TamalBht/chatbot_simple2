1e415367b3319f63435b09ab74b8fcc9453012db
Tamal Bhattacharjee <119165001+TamalBht@users.noreply.github.com>
2025-06-09 03:50:21 +0530

    changes

--- chat.py
+++ chat.py
@@ -8,28 +8,31 @@ def generate():
     client = genai.Client(
         api_key="AIzaSyC1ycYj4yGZb4n_b0jJRTQs9YavW1sP0qI",
     )
-
-    model = "gemini-1.5-flash-latest"
-    contents = [
+    while True:
+        response=input("Enter your response: ")
+        if response.lower() == "exit":
+            break
+        response=response+"keep it short and concise, no more than 100 words"
+        model = "gemini-1.5-flash-latest"
+        contents = [
         types.Content(
             role="user",
             parts=[
-                types.Part.from_text(text="""what is the color of the sky?
-                """),
+                types.Part.from_text(text=response),
             ],
         ),
     ]
-    generate_content_config = types.GenerateContentConfig(
+        generate_content_config = types.GenerateContentConfig(
         temperature=0,
         response_mime_type="text/plain",
     )
 
-    for chunk in client.models.generate_content_stream(
+        for chunk in client.models.generate_content_stream(
         model=model,
         contents=contents,
         config=generate_content_config,
     ):
-        print(chunk.text, end="")
+            print(chunk.text, end="")
 
 if __name__ == "__main__":
     generate()
